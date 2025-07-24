09318c70797c7a9730060470fda13c1e03d87a30
zezinhotwo <107373132+Zezinhotwo@users.noreply.github.com>
2025-07-23 21:37:56 -0300

    custumizacao da parte DOM do game para evento de start

--- batalhaNv/controleDOM.js
+++ batalhaNv/controleDOM.js
@@ -10,31 +10,39 @@ export default class DomControle {
         this.put_ship();
     }
 
-    addBorad(player) {
+    DOMaddBorad(name, player) {
         const tabu = document.querySelector(".tabu");
+        const board = document.createElement("div");
+        const nickname = document.createElement("div");
+        nickname.innerText = name;
+        board.classList.add("quadro")
         for (let i = 0; i < 10; i++) {
             for (let j = 0; j < 10; j++) {
                 const div = document.createElement("div");
-                if (player[i][j] !== 0) {
-                    div.dataset.point = player[i][j];
+                div.classList.add("ponto")
+                if (player[i][j] !== 0 && name !== "computer") {
+                    // div.innerText = player[i][j];
                     div.style.background = "red"
+                    // div.dataset.point = player[i][j];
                 }
-                tabu.append(div);
+                board.append(div);
             }
         }
-
-
+        tabu.append(nickname)
+        tabu.append(board)
     }
 
     put_ship() {
 
 
         const player1 = {
+            name: "player",
             tabuleiro: quadros(),
             navios: [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
         };
 
         const player2 = {
+            name: "computer",
             tabuleiro: quadros(),
             navios: [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
         };
@@ -42,7 +50,8 @@ export default class DomControle {
         new addAutomatic(player1.tabuleiro, [...player1.navios]);
         new addAutomatic(player2.tabuleiro, [...player2.navios]);
 
-        this.addBorad([...player1.tabuleiro]);
+        this.DOMaddBorad(player1.name, [...player1.tabuleiro]);
+        this.DOMaddBorad(player2.name, [...player2.tabuleiro]);
 
     }
 
--- batalhaNv/main.js
+++ batalhaNv/main.js
@@ -1,30 +1,8 @@
 import DomControle from "./controleDOM.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    new DomControle().init();
+    document.querySelector("#startGame").addEventListener("click", () => {
+            new DomControle().init();
+    });
+
 })
-
-// const tabu = document.querySelector(".tabu");
-
-// for (let i = 0; i < 10; i++) {
-//     for (let j = 0; j < 10; j++) {
-//         const indice = document.createElement("div");
-
-//         indice.classList.add("ponto");
-
-//         indice.dataset.matrs = [i, j];
-//         //   indice.dataset.j = j;
-
-//         indice.textContent = `[${i},${j}]`;
-
-//         tabu.append(indice);
-//     }
-// }
-// tabu.addEventListener("click", (event) => {
-//     const click = event.target
-//     if (click.classList.contains("ponto")) {
-//         click.style.background = "red"
-//         board(click.dataset.matrs)
-//     }
-//     console.log(event.target)
-// })
